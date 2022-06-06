cb7240da9e0f4a90372321f0690fd3fb8f1c9495
Camprite <u19_dawidborkowski1@zsp1.siedlce.pl>
2022-06-06 19:29:17 +0200

    Template

--- Czat/node.js
+++ Czat/node.js
@@ -1,1 +1,12 @@
-console.log("dzien dobry")
+const express = require('express')
+const app = express()
+const path  = require('path')
+app.use(express.static("public"));
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'index.html'))
+});
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+  });
